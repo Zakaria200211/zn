f3d971e901b3b4ece641e8be67749c0719ed5b08
Zakaria200211 <155495240+Zakaria200211@users.noreply.github.com>
2024-01-20 00:38:19 +0100

    Add files via upload

---
+++ Tik.py
@@ -0,0 +1,116 @@
+#Dec bY xF: @DevEviI  oN: @J6_10 🇵🇬 . 
+
+
+
+foo = False
+if foo:
+    pass
+from datetime import datetime
+current_time = datetime.now()
+expiry_time = datetime.strptime('''2023-12-21 00:00:00.000''', '''%Y-%m-%d %H:%M:%S.%f''')
+if current_time > expiry_time:
+    print('''[!] This file is expired''')
+    exit(0)
+import pyfiglet
+import time
+import os
+import sys
+import requests
+import random
+import threading
+E = '''[1;31m'''
+Z = '''[1;31m'''
+R = '''[1;31m'''
+X = '''[1;33m'''
+F = '''[2;32m'''
+C = '''[1;97m'''
+B = '''[2;36m'''
+Y = '''[1;34m'''
+E = '''[1;31m'''
+B = '''[2;36m'''
+G = '''[1;32m'''
+S = '''[1;33m'''
+C1 = '''[2;35m'''
+G = '''[1;35m'''
+Z = '''[1;31m'''
+X = '''[1;33m'''
+Z1 = '''[2;31m'''
+F = '''[2;32m'''
+A = '''[2;34m'''
+C = '''[2;35m'''
+B = '''[2;36m'''
+Y = '''[1;34m'''
+M = '''[1;37m'''
+S = '''[1;33m'''
+U = '''[1;37m'''
+BRed = '''[1;31m'''
+BGreen = '''[1;32m'''
+BYellow = '''[1;33m'''
+BBlue = '''[1;34m'''
+BPurple = '''[1;35m'''
+BCyan = '''[1;36m'''
+BWhite = '''[1;37m'''
+yso = '''1230'''
+print('————————————————————————————————————————————————————————————')
+print('• DeCoDe By @M_L_P_0 •')
+print('————————————————————————————————————————————————————————————')
+pss = input('''[1;31m [~]ENTER PASSWORD :''')
+if pss == yso:
+    print('''[2;32m         SUCCESS PASSWORD ''')
+    time.sleep(1)
+    os.system('''clear''')
+else:
+    exit('''             WORNG PASSWORD ''')
+IQ = pyfiglet.figlet_format('''K R A R''')
+print(IQ)
+print('————————————————————————————————————————————————————————————')
+print('• DeCoDe By @M_L_P_0 •')
+print('————————————————————————————————————————————————————————————')
+print(B + '⋘────━𓆩KRAR𓆪━────⋙')
+token = input(f'''{Z} [{B}⌯{Z}] {R}التوكن ➪  ''' + B)
+print(B + '⋘────━𓆩HACKER𓆪━────⋙')
+ID = input(f'''{Z} [{M}⌯{Z}] {R}الايدي ➪ ''' + B)
+print(B + '⋘────━𓆩KRAR𓆪────⋙')
+
+def sui():
+    
+    try:
+        user = '''1234567890.asdfghjklqwertyuiopzxcvbnm'''
+        num = ('''Michael''', '''Christopher''', '''Jessica''', '''Matthew''', '''Ashley''', '''Jennifer''', '''Joshua''', '''Amanda''', '''David''', '''Daniel''', '''James''', '''Robert''', '''John''', '''Joseph''', '''Andrew''', '''Ryan''', '''Brandon''', '''Jason''', '''Justin''', '''Sarah''', '''William''', '''Jonathan''', '''Stephanie''', '''Brian''', '''Nicole''', '''Nicholas''', '''Anthony''', '''Heather''', '''Elizabeth''', '''Megan''', '''Adam''', '''Eric''', '''Melissa''', '''Kevin''', '''Steven''', '''Thomas''')
+        '''dec by hsm'''
+        rang = None((lambda .0 = None: for i in .0:
+random.choice(num))(range(1)))
+        name = None((lambda .0 = None: for i in .0:
+random.choice(user))(range(6)))
+        username = name
+        email = name + '''@gmail.com'''
+        res = requests.get(f'''https://GmailandTiktokandzaid.zaidbot.repl.co/2/email={email}''').text
+        if '''"Dev":"@t_7_55","Email":"Good"''' in res:
+            print(f'''{F}Good Email : {email}''')
+        api = requests.get(f'''https://api.dlyar-dev.tk/info-tiktok.json?user={username}''').json()
+        if api['''status''']:
+            name = api['''name''']
+            followers = api['''followers''']
+            following = api['''following''']
+            like = api['''likes''']
+            id = api['''id''']
+            code = api['''code-country''']
+            country = api['''country''']
+            tlg = f'''\n الهم صلي علئ محمد واله محمد🀼⋘────━𓆩KRAR𓆪────⋙\n𝙽𝙰𝙼𝙴 🇮🇶 : {name}\n𝚄𝚂𝙴𝚁 𝙽𝙰𝙼𝙴 🇮🇶 : {username}\n𝙶𝙼𝙰𝙸𝙻 🇮🇶 : {email}\n𝙵𝙾𝙻𝙻𝙾𝚆𝙴𝚁𝚂 🇮🇶 : {followers}\n𝙵𝙾𝙻𝙻𝙾𝙸𝙽𝙶 🇮🇶 : {following}\n𝙻𝙸𝙺𝙴 🇮🇶 : {like}\n𝙸𝙳 🇮🇶 : {id}\n𝚄𝚁𝙻 🇮🇶 : https://www.tiktok.com/@{username}\n⋘────━𓆩KRAR𓆪━────⋙\nلاتنسى صور الصيد عمري 🥲🥀 @M_L_P_0  '''
+            tlg = f'''\n الهم صلي علئ محمد واله محمد🀼⋘────━𓆩KRAR𓆪━────⋙\n𝙽𝙰𝙼𝙴 🇮🇶 : {name}\n𝚄𝚂𝙴𝚁 𝙽𝙰𝙼𝙴 🇮🇶 : {username}\n𝙶𝙼𝙰𝙸𝙻 🇮🇶 : {email}\n𝙵𝙾𝙻𝙻𝙾𝚆𝙴𝚁𝚂 🇮🇶 : {followers}\n𝙵𝙾𝙻𝙻𝙾𝙸𝙽𝙶 🇮🇶 : {following}\n𝙻𝙸𝙺𝙴 🇮🇶 : {like}\n𝙸𝙳 🇮🇶 : {id}\n𝚄𝚁𝙻 🇮🇶 : https://www.tiktok.com/@{username}\n⋘────━𓆩KRAR𓆪━────⋙\nلاتنسى صور الصيد عمري 🥲🥀 @M_L_P_0'''
+            requests.get('''https://api.telegram.org/bot''' + str(token) + '''/sendMessage?chat_id=''' + str(ID) + '''&text=''' + str(tlg))
+            print(tlg)
+        else:
+            print(f''' Bad Gmail : {email}''')
+    except:
+        pass
+    print(f''' Bad Gmail : {email}''')
+    
+
+
+sui()
+
+
+
+#Dec bY xF: @DevEviI  oN: @J6_10 🇵🇬 . 
+
